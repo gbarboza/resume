e5dc54b5ce120935143c49ea11312a0ad92097d9
Garrett Barboza <garrett@garrettbarboza.com>
2014-01-01 18:08:49 -0500

    Remove dead code

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -e
 mkdir build && cp resume.tex build && cd build
 
 wget http://mirrors.ctan.org/macros/latex/contrib/moderncv.zip
-#wget http://mirror.unl.edu/ctan/macros/latex/contrib/moderncv/tweaklist.sty
 
 7z x moderncv.zip
 cp moderncv/moderncv.cls .
